9b1f1f86a91d9e2e22680a73a2876b4cfb277e2c
Karim <adila.karim@capgemini.com>
2024-02-09 10:57:32 -0600

    First commit

---
+++ src/main/java/org/adilakarim/SeleniumFramework/App.java
@@ -0,0 +1,13 @@
+package org.adilakarim.SeleniumFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
